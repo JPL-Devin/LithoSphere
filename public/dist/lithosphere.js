9f8697fd00c5a0a97c041d4d4a71b63c7286cb09
tariqksoliman <tariqksoliman@gmail.com>
2026-04-16 18:06:55 -0700

    Upgrade webpack to work with node v22

--- public/dist/lithosphere.js
+++ public/dist/lithosphere.js
@@ -87,7 +87,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "C:\\Users\\tsoliman\\Documents\\Projects\\LithoSphere\\public/";
+/******/ 	__webpack_require__.p = "C:\\Users\\tsoliman\\Documents\\Projects\\DevinAI\\LithoSphere\\public/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
